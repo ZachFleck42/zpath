7d8d1f89ae71cc16d4f45bad87a878a1da5b996f
ZachFleck42 <zachfleck42@Gmail.com>
2023-09-19 12:55:09 -0400

    Adds label and neighbors fields to Waypoint struct. Implements generate_label and generate_waypoints functions. Small documentation tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,22 @@ use rand::Rng; // Used to randomly generate coordinates for waypoints
 
 // Waypoints have x and y components to represent latitude and longitude respectively
 // Latitude can range from -90 to 90 and longitude can range from -180 to 180
-struct Waypoint {
+struct Waypoint<'a> {
     x: f32,
     y: f32,
+    label: String,
+    neighbors: Vec<&'a Waypoint<'a>>,
 }
 
-impl Waypoint {
-    fn new() -> Self {
+impl<'a> Waypoint<'a> {
+    fn new(label: String) -> Self {
         let mut rng = rand::thread_rng();
 
         Waypoint {
             x: rng.gen_range(-90.0..=90.0),
             y: rng.gen_range(-180.0..=180.0),
+            label,
+            neighbors: Vec::new(),
         }
     }
 
@@ -24,21 +28,43 @@ impl Waypoint {
 
     fn print_DMS(&self) {
         let lat = self.x.abs(); // Convert (-) values to (+) for cleaner code; sign only relevant in determining direction
-        let lat_degrees = lat.floor(); // The whole number portion of the value represents degrees
-        let lat_minutes = (lat - lat_degrees) * 60.0; // The value to the right of the decimal, times 60, equals minutes
+        let lat_degrees = lat.floor(); // The whole number portion of the value equals degrees
+        let lat_minutes = (lat - lat_degrees) * 60.0; // The decimal portion of the value, times 60, equals minutes
         let lat_seconds = (lat_minutes - lat_minutes.floor()) * 60.0; // The decimal portion of minutes, times 60, equals seconds
-        let lat_direction: char = if self.x >= 0.0 { 'N' } else { 'S' }; // Assign the cardinal direction based on sign
+        let lat_direction = if self.x >= 0.0 { 'N' } else { 'S' }; // Assign the cardinal direction based on sign
 
         let long = self.y.abs();
         let long_degrees = long.floor();
         let long_minutes = (long - long_degrees) * 60.0;
         let long_seconds = (long_minutes - long_minutes.floor()) * 60.0;
-        let long_direction: char = if self.y >= 0.0 { 'E' } else { 'W' };
+        let long_direction = if self.y >= 0.0 { 'E' } else { 'W' };
 
         println! {"{}°{}'{:.2}\"{}, {}°{}'{:.2}\"{}", lat_degrees, lat_minutes.floor(), lat_seconds, lat_direction, long_degrees, long_minutes.floor(), long_seconds, long_direction}
     }
 }
 
-fn generate_waypoints(amt: isize) -> Vec<Waypoint> {
-    return vec![];
+fn generate_label(n: usize) -> String {
+    let mut label = String::new();
+    let mut num = n;
+
+    while num > 0 {
+        let remainder = (num - 1) % 26;
+        let char_value = (remainder as u8 + b'A') as char;
+        label.insert(0, char_value);
+        num = (num - 1) / 26;
+    }
+
+    label
+}
+
+fn generate_waypoints<'a>(amt: isize) -> Vec<Waypoint<'a>> {
+    let mut waypoints = Vec::new();
+
+    for i in 0..amt {
+        let label = generate_label(i as usize);
+        let waypoint = Waypoint::new(label);
+        waypoints.push(waypoint);
+    }
+
+    waypoints
 }
