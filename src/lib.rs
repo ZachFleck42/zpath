6d7193d717c2518629e7c9b5078f1e090c3624af
ZachFleck42 <zachfleck42@Gmail.com>
2023-09-18 21:47:31 -0400

    Begins to implement the Waypoint data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+// Waypoints have x and y components to represent latitude and longitude respectively
+// Latitude can range from -90 to 90 and longitude can range from -180 to 180
 struct Waypoint {
-    x: isize,
-    y: isize,
+    x: f32,
+    y: f32,
 }
 
-fn generate_waypoints(amt: isize) {
-    return;
+impl Waypoint {
+    fn print_DD(&self) {}
+    fn print_DMM(&self) {}
+    fn print_DMS(&self) {}
+}
+
+fn generate_waypoints(amt: isize) -> Vec<Waypoint> {
+    return vec![];
 }
